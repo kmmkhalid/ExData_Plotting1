118a5308788a8244c05342ab65e099fa0ba55479
Karla <32632576+kmmkhalid@users.noreply.github.com>
2018-03-04 11:46:29 +0800

    Update plot4.R
    Was not able to copy and paste the correct code when I chopped my main code into separate scripts for each plot. Did not check before uploading. Sorry for the carelessness.

--- plot4.R
+++ plot4.R
@@ -11,9 +11,14 @@ Date_Time <- strptime(paste(final_data$Date,final_data$Time), "%d/%m/%Y %H:%M:%S
 #Open PNG graphics device
 png("plot4.png", width=480, height=480)
 #Plot data
-with(final_data,plot(Date_Time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = ""))
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(final_data, plot(Date_Time, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+with(final_data, plot(Date_Time, Voltage, type = "l", ylab = "Voltage", xlab = "datetime"))
+with(final_data, plot(Date_Time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")) 
 points(Date_Time, final_data$Sub_metering_2, type = "l", col = "red")
 points(Date_Time, final_data$Sub_metering_3, type = "l", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(final_data, plot(Date_Time, Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime"))
 #Close graphics device
 dev.off()
